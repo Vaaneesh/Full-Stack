5a32b925a5a602abf4fff469d92f167f1f162ca7
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2024-02-19 17:26:16 +0530

    modifications made in "generic" file

--- lec2-TypeScript/dest/generic.js
+++ lec2-TypeScript/dest/generic.js
@@ -6,3 +6,18 @@ let arr1 = [1, 2, 3, 4];
 let arr2 = ["ab", "cd", "ef"];
 console.log(getFirst(arr1));
 console.log(getFirst(arr2));
+function identity(arg) {
+    // console.log(arg);
+    return arg;
+}
+// let output1=identity("vasu");
+let output1 = identity("vasu");
+// let output2=identity(44);
+let output2 = identity(44);
+console.log(output1 + " " + output2);
+function identity2(arg) {
+    // console.log(arg);
+    return arg[0];
+}
+let output3 = identity2(arr2);
+console.log(output3);
--- lec2-TypeScript/src/generic.ts
+++ lec2-TypeScript/src/generic.ts
@@ -6,3 +6,21 @@ let arr1=[1,2,3,4];
 let arr2=["ab","cd","ef"];
 console.log(getFirst(arr1));
 console.log(getFirst(arr2));
+
+
+function identity<T>(arg:T){
+    // console.log(arg);
+    return arg;
+}
+// let output1=identity("vasu");
+let output1=identity<string>("vasu");
+// let output2=identity(44);
+let output2=identity<number>(44);
+console.log(output1+" "+output2);
+
+function identity2<T>(arg:T[]){
+    // console.log(arg);
+    return arg[0];
+}
+let output3=identity2<string>(arr2);
+console.log(output3);
