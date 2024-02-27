38eb69b9edf5937574f0aed2865214794f93b5f3
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2024-02-27 17:52:44 +0530

    CRUD functions added

--- Prisma/dist/index.js
+++ Prisma/dist/index.js
@@ -11,7 +11,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
 const prisma = new client_1.PrismaClient();
-function inserData(email, password, firstname) {
+function insertData(email, password, firstname) {
     return __awaiter(this, void 0, void 0, function* () {
         let user = yield prisma.user.create({
             data: {
@@ -23,4 +23,98 @@ function inserData(email, password, firstname) {
         console.log(user);
     });
 }
-inserData("vasu@gmail.com", "vasu123", "Vasu");
+let data = [
+    { email: 'batra@gmail.com', password: '1234', firstname: 'Vansh Batra' },
+    { email: 'ashu@gmail.com', password: '3444', firstname: 'Ashu' },
+    { email: 'rawal@gmail.com', password: '1235', firstname: 'Rawal' }
+];
+function insertManyData(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const users = yield prisma.user.createMany({
+                data: data.map(entry => ({
+                    email: entry.email,
+                    password: entry.password,
+                    firstname: entry.firstname
+                }))
+            });
+            console.log(users);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    });
+}
+function find(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const user = yield prisma.user.findUnique({
+            where: {
+                id: id
+            }
+        });
+        if (user) {
+            console.log(user);
+        }
+        else
+            console.log("User not found");
+    });
+}
+function findMany() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const user = yield prisma.user.findMany({
+            where: {
+                firstname: {
+                    startsWith: 'V'
+                }
+            }
+        });
+        if (user) {
+            console.log(user);
+        }
+        else
+            console.log("User not found");
+    });
+}
+function deleteOne(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const user = yield prisma.user.delete({
+            where: {
+                id: id
+            }
+        });
+        if (user)
+            console.log("User deleted");
+        else
+            console.log("User not found");
+    });
+}
+function deleteMany() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const user = yield prisma.user.deleteMany({
+            where: {
+                firstname: "Ashu"
+            }
+        });
+        if (user)
+            console.log("user deleted");
+    });
+}
+function update(id) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const user = yield prisma.user.update({
+            where: {
+                id: id
+            },
+            data: {
+                firstname: "Rawal Bhabhi"
+            },
+        });
+    });
+}
+// insertManyData(data);
+// find(2);
+// findMany();
+// deleteOne(6);
+// deleteMany();
+update(13);
+// insertData("vasu@gmail.com","vasu123","Vasu");
--- Prisma/src/index.ts
+++ Prisma/src/index.ts
@@ -1,7 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-async function inserData(email:string,password:string,firstname:string){
+interface UserData{
+    email:string;
+    password:string;
+    firstname:string;
+}
+
+async function insertData(email:string,password:string,firstname:string){
     let user=await prisma.user.create({
         data:{
             email,
@@ -11,4 +17,83 @@ async function inserData(email:string,password:string,firstname:string){
     })
     console.log(user);
 }
-inserData("vasu@gmail.com","vasu123","Vasu");
+let data=[
+    {email:'batra@gmail.com',password:'1234',firstname:'Vansh Batra'},
+    {email:'ashu@gmail.com',password:'3444',firstname:'Ashu'},
+    {email:'rawal@gmail.com',password:'1235',firstname:'Rawal'}
+]
+async function insertManyData(data:UserData[]){
+    try{
+        const users=await prisma.user.createMany({
+            data:data.map(entry=>({
+                email:entry.email,
+                password:entry.password,
+                firstname:entry.firstname
+            }))
+        });
+        console.log(users);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+async function find(id:number){
+    const user=await prisma.user.findUnique({
+        where:{
+            id:id
+        }
+    });
+    if(user){
+        console.log(user);
+    }
+    else console.log("User not found");
+}
+async function findMany(){
+    const user=await prisma.user.findMany({
+        where:{
+            firstname:{
+                startsWith:'V'
+            }
+        }
+    });
+    if(user){
+        console.log(user);
+    }
+    else console.log("User not found");
+}
+async function deleteOne(id:number){
+    const user=await prisma.user.delete({
+        where:{
+            id:id
+        }
+    });
+    if(user)
+    console.log("User deleted");
+    else console.log("User not found");
+}
+async function deleteMany(){
+    const user=await prisma.user.deleteMany({
+        where:{
+            firstname:"Ashu"
+        }
+    });
+    if(user)
+    console.log("user deleted");
+}
+async function update(id:number){
+    const user=await prisma.user.update({
+        where:{
+            id:id
+        },
+        data:{
+            firstname:"Rawal Bhabhi"
+        },
+    });
+}
+// insertManyData(data);
+// find(2);
+// findMany();
+// deleteOne(6);
+// deleteMany();
+update(13);
+// insertData("vasu@gmail.com","vasu123","Vasu");
