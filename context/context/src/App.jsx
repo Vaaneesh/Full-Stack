71aaf33a60eb2043f4c7c1d5384ae2f8af7e640d
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2024-04-11 17:14:11 +0530

    context folder added

---
+++ context/context/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { createContext ,useContext} from 'react';
+import './App.css'
+const myContext=createContext(0);
+function App() {
+  const [count, setCount] = useState("count ki value")
+
+  return (
+    <>
+     <h1>I am the APP Component</h1>
+     <myContext.Provider value={count}>
+     <FirstChild  />
+     </myContext.Provider>
+    </>
+  )
+}
+function FirstChild(){
+  return(
+    <>
+      <h1>I am inside App</h1>
+      <SecondChild />
+    </>
+  )
+}
+function SecondChild(){
+  return(
+    <>
+      <h1>I am inside FirstChild</h1>
+      <ThirdChild/>
+    </>
+  )
+}
+function ThirdChild(){
+  const value=useContext(myContext)
+  return(
+    <>
+      <h1>I am inside SecondChild: {value}</h1>
+    </>
+  ) 
+}
+
+
+export default App
