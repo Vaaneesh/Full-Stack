ba84f6b5f582dc0ceb78aa5288ce3c4416dce213
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2024-04-29 19:47:59 +0530

    changes made

--- closure/index.js
+++ closure/index.js
@@ -1,15 +1,35 @@
-function outer(){
-    let a=10;
-    let b=20;
-    function inner(){
-        a++;
-        b++;
-        console.log(a);
-        console.log(b);
+// function outer(){
+//     let a=10;
+//     let b=20;
+//     function inner(){
+//         a++;
+//         b++;
+//         console.log(a);
+//         console.log(b);
+//     }
+//     return inner;
+// }
+// let f1=outer();
+// f1();
+// let f2=outer();
+// f2();
+function memo(){
+    let obj={};
+    function multiplyby5(inp){
+        console.log(obj)
+        if(obj.inp){
+            console.log("memo");
+            return console.log(obj.inp);
+        }
+        let res=5*inp;
+        console.log("not memo");
+        console.log(res);
+        obj[inp]=res;
     }
-    return inner;
+    return multiplyby5;
 }
-let f1=outer();
-f1();
-let f2=outer();
-f2();
+let f1=memo();
+f1(4);
+f1(4);
+f1(5);
+f1(5);
