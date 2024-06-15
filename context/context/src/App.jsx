04e2ddd1168b79c9a57c79b4c054ac58c23533ba
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2024-06-15 19:04:17 +0530

    changes made

--- context/context/src/App.jsx
+++ context/context/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import { createContext ,useContext} from 'react';
 import './App.css'
 const myContext=createContext(0);
